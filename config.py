7dae8d3532fa283d510ecdba936623ab53020b3c
wernjieyew <110628646+wernjieyew@users.noreply.github.com>
2025-12-03 14:57:53 +0800

    Update database host in config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cdego2uckoep.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
