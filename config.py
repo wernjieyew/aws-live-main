5a805250462680793726f022b8f9125e6ee68d26
wernjieyew <110628646+wernjieyew@users.noreply.github.com>
2025-12-03 15:02:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cdego2uckoep.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "pratical9bucket1"
+custombucket = "pratical9bucket"
 customregion = "us-east-1"
 
